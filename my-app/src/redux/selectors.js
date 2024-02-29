2055f5830895128d41b6d9a980c34d7b859c3c9c
Dan <danretegan@yahoo.com>
2024-02-29 16:42:39 +0200

    use createSelector
    Watch console.log now! ;)

--- my-app/src/redux/selectors.js
+++ my-app/src/redux/selectors.js
@@ -1,3 +1,4 @@
+import { createSelector } from "@reduxjs/toolkit";
 import { statusFilters } from "./constants";
 
 export const selectTasks = (state) => state.tasks.items;
@@ -8,29 +9,26 @@ export const selectError = (state) => state.tasks.error;
 
 export const selectStatusFilter = (state) => state.filters.status;
 
-export const selectVisibleTasks = (state) => {
-  // Folsim alte selectoare:
-  const todo = selectTasks(state);
-  const statusulFiltrului = selectStatusFilter(state);
+export const selectVisibleTasks = createSelector(
+  [selectTasks, selectStatusFilter],
+  (todo, statusulFiltrului) => {
+    console.log("Calc todo count Now memoized!");
 
-  console.log("Calc todo count");
+    switch (statusulFiltrului) {
+      case statusFilters.active:
+        return todo.filter((task) => !task.completed);
 
-  switch (statusulFiltrului) {
-    case statusFilters.active:
-      return todo.filter((task) => !task.completed);
+      case statusFilters.completed:
+        return todo.filter((task) => task.completed);
 
-    case statusFilters.completed:
-      return todo.filter((task) => task.completed);
-
-    default:
-      return todo;
+      default:
+        return todo;
+    }
   }
-};
+);
 
-export const selectTaskCount = (state) => {
-  const tasks = selectTasks(state);
-
-  console.log("Calculating task count");
+export const selectTaskCount = createSelector([selectTasks], (tasks) => {
+  console.log("Calculating task count. Now memoized!");
 
   return tasks.reduce(
     (count, task) => {
@@ -46,7 +44,7 @@ export const selectTaskCount = (state) => {
       completed: 0,
     }
   );
-};
+});
 
 /**Funcții selector
 Același selector poate fi utilizat în mai multe locuri din aplicație, ceea ce duce la duplicarea codului, cum ar fi în componentele TaskList, StatusFilter și TaskCounter. Pentru a evita acest lucru și pentru a structura codul și mai bine, toate funcțiile selectorului sunt declarate într-un fișier separat. De exemplu, în src/redux/selectors.js, după care sunt importate în componente. */
